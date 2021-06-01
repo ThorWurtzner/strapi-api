af036bf62351c6a77b124050ad7084fe32cc1377
Thor Würtzner <6273158@rts-365.dk>
2021-06-01 22:31:52 +0200

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6849ef858cf6f0938c9c86ace34f1237'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0a89ac8e-3cf4-4485-996b-57bc0fa1d4e7'
+};
